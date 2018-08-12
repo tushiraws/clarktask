1862db99e58cb6c3a927c1d062585c97d7f31546
Tushier Pruthi <tushir.aws@outlook.com>
2018-08-12 12:44:06 +0000

    corrected dns

--- dnsentry.sh
+++ dnsentry.sh
@@ -2,4 +2,3 @@ mystr=`/usr/local/bin/kubectl describe service guestbook | grep Ingress | cut -d
 mystring=$(echo $mystr | tr -d ' ')
 sed -i "s/OLDDNS/$mystring/g" newdnsentry1.json
 aws route53 change-resource-record-sets --hosted-zone-id "Z2PWMWU43TX8LR" --change-batch=file://newdnsentry1.json
-sed -i "s/$mystring/OLDDNS/g" newdnsentry1.json
